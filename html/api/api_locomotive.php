4be4684c785df802f7f2443243d6c046b3b64e24
Tom Homewood <tomhomewood@gmail.com>
2020-05-06 23:09:32 +0100

    Removed api/v1 in the hope api might solve the permission issue. Fuck it, I'm willing to try anything at this point.

--- html/api/api_locomotive.php
+++ html/api/api_locomotive.php
@@ -2,7 +2,7 @@
 ini_set('display_errors', 1);
 
 include_once('api_base.php');
-include_once('../../common/loader.php');
+include_once('../common/loader.php');
 
 $loader = new Loader(ROSTER_BATH_PATH);
 $locomotive = $loader->loadLocomotive($_GET['locomotive_id']);
