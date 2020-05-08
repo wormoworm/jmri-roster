0a0535edb4ddacdded79472f6363d72fb3a20c03
Tom Homewood <tomhomewood@gmail.com>
2020-05-09 00:30:42 +0100

    Locomotive detail page now fetches image using API.

--- html/show_locomotive_details.php
+++ html/show_locomotive_details.php
@@ -1,5 +1,9 @@
 <?php
 
+$rewritesAvailable = true;
+
+$imageWidth = 700;
+
 if(!isset($_GET['locomotive_id'])){
     http_response_code(422);
     echo 'Error: locomotive_id not provided';
@@ -30,7 +34,8 @@ if($locomotive==null){
         <h1 id="title"><?php echo getPageTitle($locomotive); ?></h1>
         <?php
         echo $locomotive->imageFilePath;
-        if(isset($locomotive->imageFilePath)) echo '<img id="image" src="'.$locomotive->imageFilePath.'"/>';
+        $imagePath = $rewritesAvailable ? 'api/locomotive/'.$locomotive->id.'/image/'.$imageWidth : '/api/v1/api_locomotive_image.php?locomotive_id='.$locomotive->id.'&width='.$imageWidth;
+        if(isset($locomotive->imageFilePath)) echo '<img id="image" src="'.$imagePath.'"/>';
         if(isset($locomotive->comment)) echo '<p id="comment">'.$locomotive->comment.'</p>';
         ?>
     </body>
